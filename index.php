52aca53c1a94115c68ab32df01021bb9d1544d6c
Jav <jay7134@gmail.com>
2015-12-05 11:56:04 +0500

    Remove JavaScript

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 add_action( 'wp_print_styles', 'jay_deregister_styles', 100 );
 function jay_deregister_styles() {
     wp_deregister_style( 'contact-form-7' );
+    wp_deregister_script( 'contact-form-7' );
 }
