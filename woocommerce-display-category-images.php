99d749c7bfc610e816398cb0d4cf93963aa439b4
javed-malik <jay7134@gmail.com>
2016-03-09 16:31:08 +0500

    WooCommerce – display category image on category archive

---
+++ woocommerce-display-category-images.php
@@ -0,0 +1,14 @@
+<?php
+// WooCommerce – display category image on category archive
+add_action( 'woocommerce_archive_description', 'woocommerce_category_image', 2 );
+function woocommerce_category_image() {
+    if ( is_product_category() ){
+	    global $wp_query;
+	    $cat = $wp_query->get_queried_object();
+	    $thumbnail_id = get_woocommerce_term_meta( $cat->term_id, 'thumbnail_id', true );
+	    $image = wp_get_attachment_url( $thumbnail_id );
+	    if ( $image ) {
+		    echo '<img src="' . $image . '" alt="" />';
+		}
+	}
+}
