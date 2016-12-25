36fda8e4a31e536895b2cab4a4e047c60381049a
javed-malik <jay7134@gmail.com>
2016-12-25 10:01:06 +0500

    Divi Social Icons
    Open Divi Social Icons links to new tab

---
+++ divi-social-icons.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+
+jQuery(document).ready(function($){
+
+	$('.et-social-icons a').each(function(){ 
+	
+		$(this).prop('target','_blank');
+	
+	});
+
+});
+
+</script>
